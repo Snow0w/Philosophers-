ab0a18362a7f53fcd282c470404e4a53601c79ca
Organ Manie <omanie@mi-r1.msk.21-school.ru>
2022-02-16 20:37:56 +0300

    second absolutely bold commit lllooolll

--- philo/philo.h
+++ philo/philo.h
@@ -1,4 +1,11 @@
 #ifndef PHILO_H
 # define PHILO_H
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <pthread.h>
+#include <unistd.h>
 
+typedef struct timeval	t_time;
 #endif
--- philo/test.c
+++ philo/test.c
@@ -1,6 +1,22 @@
-#include <philo.h>
+#include "philo.h"
 
+
+void	*start(void *argv)
+{
+	printf("HHello from ChinaTown and NewYorkHello from ChinaTown and NewYorkHello from ChinaTown and NewYorkHello from ChinaTown and NewYorkHello from ChinaTown and NewYorkHello from ChinaTown and NewYorkHello from ChinaTown and NewYorkHello from ChinaTown and NewYorkello from ChinaTown and NewYork\n");
+	return (NULL);
+}
 int	main(int argc, char **argv)
 {
+	t_time	time;
+	pthread_t	t[2];
+
+	pthread_create(&t[1], NULL, &start, NULL);
+	pthread_create(&t[2], NULL, &start, NULL);
+	gettimeofday(&time, NULL);
+	printf("sec: %ld\nmicrosec: %d\n", time.tv_sec, time.tv_usec);
+	usleep(1000000);
+	gettimeofday(&time, NULL);
+	printf("sec: %ld\nmicrosec: %d\n", time.tv_sec, time.tv_usec);
 	return (0);
 }
